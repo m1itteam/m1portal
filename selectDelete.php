8579fbc6c4919c1dae7973a6cd093824c37b508d
m1itteam <148028047+m1itteam@users.noreply.github.com>
2023-10-22 22:07:23 +0900

    Update selectDelete.php

--- selectDelete.php
+++ selectDelete.php
@@ -41,10 +41,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
       $deletedCellsCount = count($deletedCells);
 
+      $pattern = '/(.*?)\"(\d+),(\d+)\"/';
+
       if ($deletedCellsCount == 2) {
         $logEntry = "processStart";
         file_put_contents($logFile, $logEntry . PHP_EOL, FILE_APPEND);
         unset($homework[$deletedLineNumber]);
+
+        $parentLineNumber = $deletedLineNumber - 1;
+        for ($i = $parentLineNumber; $i >= 0; $i--) {
+          $countCells = count(explode("|",$homework[$i]));
+          if ($countCells = 3){
+            break;
+          }
+          $parentLineNumber--;
+        }
+        $parentCells = explode("|",$homework[$parentLineNumber]);
+        preg_match($pattern, $sparentCells[0], $matches);
+        $rowNumber = $matches[3] - 1;
+        $newCell = $matches[1] . '"' . $matches[2] . $rowNumber . '"';
+        array_splice($parentCells, 0, 1, $newCell);
+        $newLine = implode("|",$parentCells);
+        array_splice($homework, $parentLineNumber, 1, $newLine);
       } else {
         $logEntry = "processStart";
         file_put_contents($logFile, $logEntry . PHP_EOL, FILE_APPEND);
